4d051e9a5bcc9d4ceb78e3cf324f89803197f601
Xeno2697 <kaede3216@gmail.com>
2022-06-01 13:44:10 +0900

    master

--- 4moku/board.py
+++ 4moku/board.py
@@ -1,177 +1,145 @@
+from http.client import ImproperConnectionState
+from unittest import result
+from xml.sax import make_parser
 import numpy as np
-from more_itertools import chunked
 
-EMPTY=0
-PLAYER_X=1
-PLAYER_O=-1
-RED = '\033[91m'
-ENDC = '\033[0m'
-MARKS={PLAYER_X: RED + "X" + ENDC, PLAYER_O: "O", EMPTY: " "}
-DRAW=2
-COL_NUM = 5
-ROW_NUM = 5
-ALL_POS_COUNT = 25
+X:int = 7
+Y:int = 6
+WIN_CONDITION = 4
+mp = np.zeros((X,Y))
 
-class Board:
+last_put_x = 3 #勝敗判定に利用
+last_put_y = 4 #勝敗判定に利用
+class board:
 
-    def __init__(self,board=None):
-        if board==None:
-            self.board = []
-            for i in range(ALL_POS_COUNT):self.board.append(EMPTY)
-        else:
-            self.board=board
-        self.winner=None
+    def __init__(self):
+        mp = np.zeros((X,Y))    
 
-    def get_possible_pos(self):
-        pos = []
-        append = pos.append
-
-        for i in range(ALL_POS_COUNT):
-            if self.board[i] == EMPTY:
-                # 下のマスが埋まっている場合(重力対応)
-                below_i = i + COL_NUM
-                if below_i >= ALL_POS_COUNT or self.board[below_i] != EMPTY:
-                    append(i)
-
-        return pos
-
-    def print_board(self):
-        tempboard=[]
-        for i in self.board:
-            tempboard.append(MARKS[i])
-        row = ' {} | {} | {} | {} | {} '
-        hr = '\n-------------------\n'
-
-        matrix_str = ''
-        for i in range(ROW_NUM):
-            matrix_str += row
-            if i != ROW_NUM - 1:
-                matrix_str += hr
-
-        print(matrix_str.format(*tempboard))
-
-
-    def check_winner(self, pos):
-        self.check_winner_horizontal(pos)
-        self.check_winner_vertical()
-        self.check_winner_skew()
-        return None
-
-
-    def check_winner_horizontal(self, pos):
-        start_pos = pos - (pos % COL_NUM)
-        end_pos = start_pos + COL_NUM - 1
-
-        for i in range(start_pos, end_pos):
-            if i + COL_NUM - 1 > end_pos:
+    def capable_path(self):
+        path = []
+        for i in range(X):
+            if mp[i,Y-1] == 0:
+                path.append(i)
+        return path
+    
+    def push(x,player):
+        for i in range(Y):
+            if mp[x,i] == 0:
+                mp[x,i] = player
+                last_put_x = X
+                last_put_y = i
                 break
-            if self.board[i] == self.board[i + 1] == self.board[i + 2] == self.board[i + 3]:
-                if self.board[i] != EMPTY:
-                    self.winner = self.board[i]
-                    return self.winner
+        return mp
 
-        # ②
-        # for i in range(ALL_POS_COUNT - 4):
-        #     if i % COL_NUM < (i + 3) % COL_NUM:
-        #         if self.board[i] == self.board[i + 1] == self.board[i + 2] == self.board[i + 3]:
-        #             if self.board[i] != EMPTY:
-        #                 self.winner = self.board[i]
-        #                 return self.winner
+    def pop(x):
+        a = 0
+        for i in reversed(range(Y)):
+            if mp[x,i] != 0:
+                a = mp[x,i]
+                mp[x,i] = 0
+                break
+        return a
 
-        # ①
-        # rows = list(chunked(self.board, COL_NUM))
-        # self.check_connected(rows)
+    def show():
+        print('--',end='')
+        for i in range(X):
+            print('-',end='')
+        print('')
+        for i in reversed(range(Y)):
+            print('|',end='')
+            for j in range(X):
+                if mp[j,i] == 0:
+                    print(' ',end='')
+                elif mp[j,i] == 1:
+                    print('o',end='')
+                elif mp[j,i] == 2:
+                    print('x',end='')
+            print('|')
+        print('--',end='')
+        for i in range(X):
+            print('-',end='')
+        print('')
 
+    def reset():
+        mp = np.zeros(X,Y)
 
-    def check_winner_vertical(self):
-        rows = list(chunked(self.board, COL_NUM))
-        cols = list(zip(*rows))
-        self.check_connected(cols)
+    def tupleout():
+        return tuple(map(tuple, mp))
+    
+    def judge(x=-1,play = 1):#引数がない場合はそのまま、ある場合、配置後の仮想マップを判定
+        #最後に駒配置された縦列を勝敗確認
+        if x != -1:
+            board.push(x,play)
+        player = 0
+        count = 0
+        result = 0
+        for i in range(Y):
+            if mp[last_put_x,i] == 0:
+                count = 0
+                player = 0
+            elif mp[last_put_x,i] == player:
+                count += 1
+                if count == 4:
+                    result = player
+                    break
+            else:
+                count = 1
+                player = mp[last_put_x,i]
 
-
-    def check_winner_skew(self):
-        # indexに対応するマスに埋まっているプレイヤーの行列を作る
-        for index_list in Board.__get_check_indices():
-            if self.board[index_list[0]] == self.board[index_list[1]] == self.board[index_list[2]] == self.board[index_list[3]]:
-               if self.board[index_list[0]] != EMPTY:
-                   self.winner = self.board[index_list[0]]
-                   return self.winner
-        # self.check_connected(rows)
-
-
-    def check_connected(self, lists):
-        for list in lists:
-            pre = EMPTY
+        if result == 0:
+            #最後に駒配置された横列を勝敗確認
+            player = 0
             count = 0
-            for player in list:
-                if player == EMPTY:
+            result = 0
+            for i in range(X):
+                if mp[i,last_put_y] == 0:
                     count = 0
-                elif pre == player:
+                    player = 0
+                elif mp[i,last_put_y] == player:
                     count += 1
                     if count == 4:
-                        self.winner = player
+                        result = player
+                        break
                 else:
                     count = 1
+                    player = mp[i,last_put_y]
 
-                pre = player
+            if result == 0:
+                #\斜め確認
+                a = min(last_put_x,last_put_y)
+                b = min(X-last_put_x+a-1,Y-last_put_y+a-1)
+                for i in range(b):
+                    if mp[last_put_x-a+i,last_put_y-a+i] == 0:
+                        count = 0
+                        player = 0
+                    elif mp[last_put_x-a+i,last_put_y-a+i] == player:
+                        count += 1
+                        if count == 4:
+                            result = player
+                            break
+                    else:
+                        count = 1
+                        player = mp[last_put_x-a+i,last_put_y-a+i]
 
+                if result == 0:
+                    #/斜め確認
+                    a = min(X-1-last_put_x,last_put_y)
+                    b = min(last_put_x+a+1,Y-(last_put_y-a))
+                    for i in range(b):
+                        if mp[last_put_x+a-i,last_put_y-a+i] == 0:
+                            count = 0
+                            player = 0
+                        elif mp[last_put_x+a-i,last_put_y-a+i] == player:
+                            count += 1
+                            if count == 4:
+                                result = player
+                                break
+                        else:
+                            count = 1
+                            player = mp[last_put_x+a-i,last_put_y-a+i]
+                        #print(last_put_x+a-i,last_put_y-a+i)
+        if x != -1:
+            board.pop(x)
+        return result
 
-    def check_draw(self):
-        if len(self.get_possible_pos())==0 and self.winner is None:
-            self.winner=DRAW
-            return DRAW
-        return None
-
-    def move(self,pos,player):
-        if self.board[pos]== EMPTY:
-            self.board[pos]=player
-        else:
-            self.winner=-1*player
-        self.check_winner(pos)
-        self.check_draw()
-
-    def clone(self):
-        return Board(self.board.copy())
-
-    def switch_player(self):
-        if self.player_turn == self.player_x:
-            self.player_turn=self.player_o
-        else:
-            self.player_turn=self.player_x
-
-
-    @classmethod
-    def __get_check_indices(cls):
-        if hasattr(cls, 'check_indices'):
-            return cls.check_indices
-
-        # print('__get_check_indices')
-        indices = []
-
-        # 左上がり斜め方向のindexのリストを作る
-        for i in range(ALL_POS_COUNT):
-            list = []
-            next_i = i
-            while next_i < ALL_POS_COUNT:
-                if next_i % COL_NUM >= i % COL_NUM:
-                    list.append(next_i)
-                next_i += COL_NUM + 1
-
-            if len(list) >= 4:
-                indices.append(list)
-
-        # 右上がり斜め方向のindexのリストを作る
-        for i in range(ALL_POS_COUNT):
-            list = []
-            next_i = i
-            while next_i < ALL_POS_COUNT:
-                if (next_i % COL_NUM < i % COL_NUM) or i == next_i:
-                    list.append(next_i)
-                next_i += COL_NUM - 1
-
-            if len(list) >= 4:
-                indices.append(list)
-
-        cls.check_indices = indices
-        # print(indices)
-        return cls.check_indices
+board.show()
